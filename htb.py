f8dbcc718cc924c3b6eabb3e233fc92ac2d9a62e
weasley1x <alihamzadirgen0102@gmail.com>
2024-07-19 22:03:06 +0300

    Add files via upload

---
+++ htb.py
@@ -0,0 +1,12 @@
+logo = '''.##.....##.########.########.
+.##.....##....##....##.....##
+.##.....##....##....##.....##
+.#########....##....########.
+.##.....##....##....##.....##
+.##.....##....##....##.....##
+.##.....##....##....######## '''
+print = ('')
+line = '**************************************'
+number = input ("Enter Target Number >")
+sunbr = input ("The number you entered has been sent to our servers. Enter your own phone number so that we can forward it to you when the information is available >")
+THX =  input("THX FOR USING SIR, HAVE A GOOD DAY!")
